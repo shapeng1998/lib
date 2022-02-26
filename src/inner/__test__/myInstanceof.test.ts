27a5d3a653cfead999f690ff79c0e87f936e6399
Jack <jack@shapeng1998.com>
2022-02-26 16:11:19 +0800

    chore: update test function of myInstanceof

--- src/inner/__test__/myInstanceof.test.ts
+++ src/inner/__test__/myInstanceof.test.ts
@@ -1,11 +1,13 @@
 import { describe, expect, it } from 'vitest';
 import { myInstanceof } from '../myInstanceof';
 
+class Cat {}
+
 describe('Instanceof inner function', () => {
   it('should check instanceof', () => {
-    const arr = new Array();
-    expect(myInstanceof(arr, Array)).toBe(arr instanceof Array);
-    expect(myInstanceof(arr, Object)).toBe(arr instanceof Object);
-    expect(myInstanceof(arr, Function)).toBe(arr instanceof Function);
+    const cat = new Cat();
+    expect(myInstanceof(cat, Cat)).toBe(true);
+    expect(myInstanceof(cat, Object)).toBe(true);
+    expect(myInstanceof(cat, Function)).toBe(false);
   });
 });
