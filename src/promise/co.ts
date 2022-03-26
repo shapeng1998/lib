9367c24a8d2e3d7a0a4d794d3a9c51b3bdf6f74b
Jack <jack@shapeng1998.com>
2022-03-26 20:44:19 +0800

    feat: add co function

---
+++ src/promise/co.ts
@@ -0,0 +1,33 @@
+export function co(fn: () => Generator) {
+  return new Promise((resolve, reject) => {
+    const gen = fn();
+    onfulfilled();
+
+    function next(result: IteratorResult<unknown, any>) {
+      const { value, done } = result;
+      if (done) {
+        return resolve(value);
+      }
+
+      Promise.resolve(value).then(onfulfilled, onrejected);
+    }
+
+    function onfulfilled(value?: any) {
+      try {
+        const result = gen.next(value);
+        next(result);
+      } catch (e) {
+        reject(e);
+      }
+    }
+
+    function onrejected(reason?: any) {
+      try {
+        const result = gen.throw(reason);
+        next(result);
+      } catch (e) {
+        reject(e);
+      }
+    }
+  });
+}
